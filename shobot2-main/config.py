3c6dd25220385483c873433b3735fb015da1a8d3
hueyfreeman-alt <toxicaurayt8@gmail.com>
2026-06-16 20:00:20 +0300

    Delete shobot2-main directory

--- shobot2-main/config.py
+++
@@ -1,2 +0,0 @@
-TOKEN = os.getenv("TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
--- shobot2-main/ui/keyboards.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-def main_menu():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="🛍 Shop", callback_data="shop")],
-        [InlineKeyboardButton(text="🛒 Cart", callback_data="cart")]
-    ])
